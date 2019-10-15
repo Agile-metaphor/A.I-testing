0595738d08743449c5a1725f83eb2ac725cb12e6
daddyboy77 <53454327+daddyboy77@users.noreply.github.com>
2019-10-15 18:42:07 +0100

    Add files via upload
    Just click on A.I.exe if you want to see it work or .sln if you want to edit it in visual studio

---
+++ A.I/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace A.I
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 30);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(153, 117);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(189, 30);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(153, 117);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 177);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "State: Awake";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Input";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(189, 8);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Output";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(354, 234);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "A.I";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ A.I/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+using System.Speech.Recognition;
+using System.Diagnostics;
+using System.Xml;
+
+namespace A.I
+{
+
+    public partial class Form1 : Form
+    {
+        SpeechSynthesizer s = new SpeechSynthesizer();
+        Boolean wake = false;
+        Choices list = new Choices();
+        String temp, condition,low,high;
+
+        public Form1()
+        {
+            SpeechRecognitionEngine rec = new SpeechRecognitionEngine();
+
+            list.Add(new String[] {"Cortin", "hello", "how are you", "what time is it", "what day is it", "open google", "open youtube"
+                , "wake", "sleep", "Cortin wake up","restart","update","open universe sandbox 2", "close universe sandbox 2",
+                "whats the weather like","cortin"
+            });
+
+
+            Grammar gram = new Grammar(new GrammarBuilder(list));
+            try
+            {
+                rec.RequestRecognizerUpdate();
+                rec.LoadGrammar(gram);
+                rec.SpeechRecognized += rec_SpeechRecognized;
+                rec.SetInputToDefaultAudioDevice();
+                rec.RecognizeAsync(RecognizeMode.Multiple);
+            }
+            catch { return; }
+
+
+            InitializeComponent();
+            s.SelectVoiceByHints(VoiceGender.Female, VoiceAge.Adult);
+            s.Speak("Welcome");
+        }
+    
+        
+        public static void closeProg(string t)
+        {
+            System.Diagnostics.Process[] procs = null;
+
+            try
+            {
+                procs = Process.GetProcessesByName(t);
+                Process prog = procs[0];
+
+                if (!prog.HasExited)
+                {
+                    prog.Kill();
+                }
+            }
+            finally
+            {
+                if(procs != null)
+                {
+                    foreach(Process p in procs)
+                    {
+                        p.Dispose();
+                    }
+                }
+            }
+
+
+        } 
+
+        public void say(String h)
+        {
+            s.Speak(h);
+            wake = false;
+            textBox2.AppendText(h + "\n");
+        }
+        public void restart()
+        {
+            Process.Start(@"C:\Users\Baba\Desktop\Movement_Energy Calc\A.I/A.I.exe");
+            Environment.Exit(0);
+        }
+        String[] jokes = new String[7]
+        {   "A perfectionist walked into a bar..apparently, the bar wasn’t set high enough.",
+            "Why do we tell actors to “break a leg?”..Because every play needs a cast",
+            "I invented a new word!... Plagiarism!",
+            "",
+            "",
+            "",
+            ""
+        };
+        public void unisand2()
+        {
+            Process.Start(@"C:\Users\Baba\Desktop\Universe.Sandbox.2.v23.1.2\Universe Sandbox x64.exe");
+        }
+        //cccommands commandscommandscommandscommandscommandscommandscommandscommandscommandscommandscommandscommandscommandscommandscommandscommandscommandscommandscommandscommandscommandscommandscommands
+       
+        private void rec_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            String r = e.Result.Text;
+            if(r == "cortin")
+            {
+                wake = true;
+            }
+
+            if (r == "Cortin wake up")
+            {
+                wake = true;
+                label3.Text = "State: Awake";
+            }
+            if (r == "wake")
+            {
+                wake = true;
+                label3.Text = "State: Awake";
+            }
+            if (r == "sleep")
+            {
+                wake = false;
+                label3.Text = "State: Sleeping";
+            }
+            if (wake == true)
+
+
+
+
+            {
+                if (r == "update" || r == "restart")
+                {
+                    restart();
+                }
+
+
+                if (r == "Cortin")
+                {
+                    say("yes how can i help you");
+                }
+                //What U say 
+                if (r == "hello")
+                {
+                    //What it should  say
+                    say("hi");
+                }
+                if (r == "how are you")
+                {
+                    //What it should say
+                    say("Fine");
+                }
+                if (r == "what time is it")
+                {
+                    say(DateTime.Now.ToString("M h:m tt"));
+                }
+                if (r == "what day is it")
+                {
+                    say(DateTime.Now.ToString("M/d/yyyy"));
+                }
+                if (r == "open youtube")
+                {
+                    say("opening youtube");
+                    Process.Start("http://youtube.com");
+                }
+                if (r == "open google")
+                {
+                    say("opening google");
+                    Process.Start("http://google.com");
+                }
+                if (r == "open universe sandbox 2")
+                {
+                    say("opening universe sandox 2");
+                    unisand2();
+                }
+                if (r == "close universe sandbox 2")
+                {
+                    say("closing universe sandox 2");
+                    closeProg("Universe Sandbox x64.exe");
+                }
+
+            }   
+            textBox1.AppendText(r + "\n");
+        }
+        
+
+        private void  Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
